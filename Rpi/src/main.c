30f8f35c069625974a1e7b98f2160fa113f1c8e4
Guy Wilson <wilson.guy@gmail.com>
2019-05-31 22:34:35 +0100

    Add cmd line parameters

--- Rpi/src/main.c
+++ Rpi/src/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <errno.h>
 #include <fcntl.h> 
@@ -7,7 +9,6 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <stdio.h>
 
 #include "rxtxmsgdef.h"
 
@@ -255,21 +256,122 @@ void * queryTPHThread(void * pArgs)
 	return NULL;
 }
 
+int mapBaudRate(int baud)
+{
+	int		baudConst = B9600;
+
+	switch (baud) {
+		case 50:
+			baudConst = B50;
+			break;
+
+		case 75:
+			baudConst = B75;
+			break;
+
+		case 110:
+			baudConst = B110;
+			break;
+
+		case 134:
+			baudConst = B134;
+			break;
+
+		case 150:
+			baudConst = B150;
+			break;
+
+		case 200:
+			baudConst = B200;
+			break;
+
+		case 300:
+			baudConst = B300;
+			break;
+
+		case 600:
+			baudConst = B600;
+			break;
+
+		case 1200:
+			baudConst = B1200;
+			break;
+
+		case 1800:
+			baudConst = B1800;
+			break;
+
+		case 2400:
+			baudConst = B2400;
+			break;
+
+		case 4800:
+			baudConst = B4800;
+			break;
+
+		case 9600:
+			baudConst = B9600;
+			break;
+
+		case 19200:
+			baudConst = B19200;
+			break;
+
+		case 38400:
+			baudConst = B38400;
+			break;
+
+		case 57600:
+			baudConst = B57600;
+			break;
+
+		case 115200:
+			baudConst = B115200;
+			break;
+
+		case 230400:
+			baudConst = B230400;
+			break;
+
+		default:
+			printf("Unsupported baud rate [%d], supported rates are 50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400\n", baud);
+			printf("Defaulting to 9600 baud\n");
+			break;
+	}
+
+	return baudConst;
+}
+
 int main(int argc, char *argv[])
 {
 	int			err;
 	pthread_t	tid;
 	char		szPort[128];
+	char		szBaud[8];
+	int			baud;
 	int			fd;
+	int			i;
 
 	if (argc > 1) {
-		strcpy(szPort, &argv[1][0]);
+		for (i = 1;i < argc;i++) {
+			if (argv[i][0] == '-') {
+				if (strcmp(&argv[i][1], "port") == 0) {
+					strcpy(szPort, &argv[++i][0]);
+				}
+				else if (strcmp(&argv[i][1], "baud") == 0) {
+					strcpy(szBaud, &argv[++i][0]);
+					baud = mapBaudRate(atoi(szBaud));
+				}
+			}
+		}
 	}
 	else {
-		strcpy(szPort, SERIAL_PORT);
+		printf("Usage:\n");
+		printf("\twctl -port [serialPort] -baud [baudrate]\n\n");
+		return -1;
 	}
 
-	fd = openSerialPort(szPort, B115200);
+	fd = openSerialPort(szPort, baud);
 
 	if (fd < 0) {
 		return -1;
