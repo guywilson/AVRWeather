79dfc335001307b0f0c55e9f813a7daabf316cb5
Guy Wilson <wilson.guy@gmail.com>
2019-06-24 19:58:46 +0100

    Updated listen port

--- web-app/server.js
+++ web-app/server.js
@@ -204,6 +204,6 @@ app.post('/api/max-tph', function(req, res) {
 	res.json(["OK", ""]);
 })
 
-app.listen(3000, function () {
-  console.log('Weather app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Weather app listening on port 80!');
 })
