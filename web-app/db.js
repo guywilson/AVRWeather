ee7ee41cba66bc9abd988d493c1b1d61df5a20a5
Guy Wilson <wilson.guy@gmail.com>
2019-07-05 20:35:28 +0100

    Testing

---
+++ web-app/db.js
@@ -0,0 +1,54 @@
+const Postgres = require('pg');
+
+const Pool = Postgres.Pool;
+const pool = new Pool({
+    user: 'web-app',
+    host: 'localhost',
+    database: 'web-app',
+    password: 'Jup1ter',
+    post: 5432
+});
+
+function getChartData_24h(callback) {
+    pool.query('SELECT * FROM TPH WHERE TYPE = "AVG" ORDER BY TIMESTAMP DESC LIMIT 24', (error, results) => {
+        if (error) {
+            console.log("Error selecting chart data");
+            throw error;
+        }
+
+        return callback(results.rows);
+    });
+};
+
+function getChartData_7d(callback) {
+    pool.query('SELECT * FROM TPH WHERE TYPE = "AVG" AND EXTRACT (\'hour\' from TS) IN (0, 6, 12, 18) ORDER BY TIMESTAMP DESC LIMIT 28', (error, results) => {
+        if (error) {
+            console.log("Error selecting chart data");
+            throw error;
+        }
+
+        return callback(results.rows);
+    });
+};
+
+function getChartData_28d(callback) {
+    pool.query('SELECT * FROM TPH WHERE TYPE = "AVG" AND EXTRACT (\'hour\' from TS) = 12 ORDER BY TIMESTAMP DESC LIMIT 28', (error, results) => {
+        if (error) {
+            console.log("Error selecting chart data");
+            throw error;
+        }
+
+        return callback(results.rows);
+    });
+};
+
+function putChartData(ts, type, temperature, pressure, humidity) {
+    console.log('Inserting record');
+    
+    pool.query('INSERT INTO TPH (ts, type, temperature, pressure, humidity) VALUES ($1, $2, $3, $4, $5)', [ts, type, temperature, pressure, humidity], (error, results) => {
+        if (error) {
+            console.log("Error inserting chart data");
+            throw error;
+        }
+    });
+};
--- web-app/server.js
+++ web-app/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const db = require('./db');
+
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
@@ -28,21 +30,114 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-function getChartData(callback) {
+function getAllMongoData(callback) {
 	MongoClient.connect(mongoURL, function(error, client) {
 		if (error) {
 			throw err;
 		}
 	
+		var db = client.db('WeatherDB');
+		var collection = db.collection('AverageTPH');
+
+		var query = {};
+		
+		collection.find(query).toArray((error, items) => {
+			if (error) {
+				throw error;
+			}
+			
+			return callback(items);
+		});
+	
+		client.close();
+	});
+}
+
+function getChartData_24h(callback) {
+	MongoClient.connect(mongoURL, function(error, client) {
+		if (error) {
+			throw err;
+		}
+	
+		var db = client.db('WeatherDB');
+		var collection = db.collection('AverageTPH');
+
+		var query = {};
+	
 		var options = {
 			"limit": 24
 		};
+		
+		collection.find(query, options).sort({timestamp: -1}).toArray((error, items) => {
+			if (error) {
+				throw error;
+			}
+			
+			return callback(items);
+		});
+	
+		client.close();
+	});
+}
+
+function getChartData_7d(callback) {
+	MongoClient.connect(mongoURL, function(error, client) {
+		if (error) {
+			throw err;
+		}
 	
 		var db = client.db('WeatherDB');
-
 		var collection = db.collection('AverageTPH');
+
+		var query = {
+			"$or": [
+				{ "timestamp": /.* 06:.*/ }, 
+				{ "timestamp": /.* 12:.*/ }, 
+				{ "timestamp": /.* 18:.*/ }, 
+				{ "timestamp": /.* 00:.*/ }
+			]
+		};
+	
+		var options = {
+			"limit": 28
+		};
 		
-		collection.find({}, options).sort({timestamp: -1}).toArray((error, items) => {
+		/*
+		** Get a reading 4 times a day at 06:xx, 12:xx, 18:xx, 00:xx for 7 days...
+		*/
+		collection.find(query, options).sort({timestamp: -1}).toArray((error, items) => {
+			if (error) {
+				throw error;
+			}
+			
+			return callback(items);
+		});
+	
+		client.close();
+	});
+}
+
+function getChartData_28d(callback) {
+	MongoClient.connect(mongoURL, function(error, client) {
+		if (error) {
+			throw err;
+		}
+	
+		var db = client.db('WeatherDB');
+		var collection = db.collection('AverageTPH');
+
+		var query = {
+			timestamp: "/.* 12:.*/"
+		};
+	
+		var options = {
+			"limit": 28
+		};
+		
+		/*
+		** Get a reading once a day at noon for 28 days...
+		*/
+		collection.find(query, options).sort({timestamp: -1}).toArray((error, items) => {
 			if (error) {
 				throw error;
 			}
@@ -73,38 +168,103 @@ app.get('/', function (req, res) {
 			});
 })
 
+app.get('/copy', function (req, res) {
+	console.log('Copying data from MongoDB to Postgresql');
+
+	getAllMongoData(function(items) {
+		items.forEach(function(item, index) {
+			db.putChartData(item.timestamp, 'AVG', item.temperature, item.pressure, item.humidity);
+		});
+	});
+})
 /*
 ** Render charts page...
 */
 app.get('/charts', function (req, res) {
-	var xLabels = [23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+	var xLabels = [];
 	var tempReadings = [];
 	var pressureReadings = [];
 	var humidityReadings = [];
 
-	getChartData(function(items) {
-		items.forEach(function(item, index) {
-			//xLabels = xLabels.concat('\'' + item.timestamp + '\'');
-			tempReadings = tempReadings.concat(item.temperature);
-			pressureReadings = pressureReadings.concat(item.pressure);
-			humidityReadings = humidityReadings.concat(item.humidity);
-		});
+	if (req.query.period == '24h') {
+		xLabels = [23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
-		/*
-		** We want the readings the other way around, with the latest
-		** reading last...
-		*/
-		tempReadings = tempReadings.reverse();
-		pressureReadings = pressureReadings.reverse();
-		humidityReadings = humidityReadings.reverse();
-
-		res.render('charts', {
-					xLabels: xLabels,
-					tempReadings: tempReadings,
-					pressureReadings: pressureReadings,
-					humidityReadings: humidityReadings
+		db.getChartData_24h(function(items) {
+			items.forEach(function(item, index) {
+				tempReadings = tempReadings.concat(item.temperature);
+				pressureReadings = pressureReadings.concat(item.pressure);
+				humidityReadings = humidityReadings.concat(item.humidity);
+			});
+	
+			/*
+			** We want the readings the other way around, with the latest
+			** reading last...
+			*/
+			tempReadings = tempReadings.reverse();
+			pressureReadings = pressureReadings.reverse();
+			humidityReadings = humidityReadings.reverse();
+	
+			res.render('charts', {
+						xLabels: xLabels,
+						tempReadings: tempReadings,
+						pressureReadings: pressureReadings,
+						humidityReadings: humidityReadings
+			});
 		});
-	});
+	}
+	else if (req.query.period == '7d') {
+		xLabels = [6, 5, 4, 3, 2, 1, 0];
+
+		db.getChartData_7d(function(items) {
+			items.forEach(function(item, index) {
+				console.log("Timestamp: " + item.timestamp);
+				tempReadings = tempReadings.concat(item.temperature);
+				pressureReadings = pressureReadings.concat(item.pressure);
+				humidityReadings = humidityReadings.concat(item.humidity);
+			});
+	
+			/*
+			** We want the readings the other way around, with the latest
+			** reading last...
+			*/
+			tempReadings = tempReadings.reverse();
+			pressureReadings = pressureReadings.reverse();
+			humidityReadings = humidityReadings.reverse();
+	
+			res.render('charts', {
+						xLabels: xLabels,
+						tempReadings: tempReadings,
+						pressureReadings: pressureReadings,
+						humidityReadings: humidityReadings
+			});
+		});
+	}
+	else if (req.query.period == '28d') {
+		xLabels = [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+		db.getChartData_28d(function(items) {
+			items.forEach(function(item, index) {
+				tempReadings = tempReadings.concat(item.temperature);
+				pressureReadings = pressureReadings.concat(item.pressure);
+				humidityReadings = humidityReadings.concat(item.humidity);
+			});
+	
+			/*
+			** We want the readings the other way around, with the latest
+			** reading last...
+			*/
+			tempReadings = tempReadings.reverse();
+			pressureReadings = pressureReadings.reverse();
+			humidityReadings = humidityReadings.reverse();
+	
+			res.render('charts', {
+						xLabels: xLabels,
+						tempReadings: tempReadings,
+						pressureReadings: pressureReadings,
+						humidityReadings: humidityReadings
+			});
+		});
+	}
 })
 
 /*
@@ -119,23 +279,7 @@ app.post('/api/avg-tph', function(req, res) {
 
 	if (doSave == 'true') {
 //		console.log('Saving average TPH data to database');
-		
-		MongoClient.connect(mongoURL, function(error, client) {
-			if (error) {
-				throw err;
-			}
-			
-			var db = client.db('WeatherDB');
-			
-		    db.collection('AverageTPH').insertOne({
-		        timestamp: timestamp,
-		        temperature: avgTemperature,
-				pressure: avgPressure,
-				humidity: avgHumidity
-		    });
-	
-			client.close();
-		});
+		db.putChartData(timestamp, 'AVG', avgTemperature, avgPressure, avgHumidity);		
 	}
 	 	
 //	console.log('Received AVG TPH data. Save = ' + doSave + 'T = ' + avgTemperature + ' P = ' + avgPressure + ' H = ' + avgHumidity);
@@ -155,25 +299,9 @@ app.post('/api/min-tph', function(req, res) {
 
 	if (doSave == 'true') {
 //		console.log('Saving minimum TPH data to database');
-		
-		MongoClient.connect(mongoURL, function(error, client) {
-			if (error) {
-				throw err;
-			}
-			
-			var db = client.db('WeatherDB');
-			
-		    db.collection('MinimumTPH').insertOne({
-		        timestamp: timestamp,
-		        temperature: minTemperature,
-				pressure: minPressure,
-				humidity: minHumidity
-		    });
-	
-			client.close();
-		});
+		db.putChartData(timestamp, 'MIN', avgTemperature, avgPressure, avgHumidity);		
 	}
-	 	
+				 
 //	console.log('Received MIN TPH data. Save = ' + doSave + 'T = ' + avgTemperature + ' P = ' + avgPressure + ' H = ' + avgHumidity);
 	
 	res.json(["OK", ""]);
@@ -191,24 +319,8 @@ app.post('/api/max-tph', function(req, res) {
 
 	if (doSave == 'true') {
 //		console.log('Saving maximum TPH data to database');
-		
-		MongoClient.connect(mongoURL, function(error, client) {
-			if (error) {
-				throw err;
-			}
-			
-			var db = client.db('WeatherDB');
-			
-		    db.collection('MaximumTPH').insertOne({
-		        timestamp: timestamp,
-		        temperature: maxTemperature,
-				pressure: maxPressure,
-				humidity: maxHumidity
-		    });
-	
-			client.close();
-		});
-	}
+		db.putChartData(timestamp, 'MAX', avgTemperature, avgPressure, avgHumidity);		
+}
 	 	
 //	console.log('Received MAX TPH data. Save = ' + doSave + 'T = ' + avgTemperature + ' P = ' + avgPressure + ' H = ' + avgHumidity);
 	
