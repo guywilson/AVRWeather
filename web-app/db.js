e31e2305b5285091db8019c05f8a08e6e07ac33f
Guy Wilson <wilson.guy@gmail.com>
2019-07-06 08:51:17 +0100

    Removed semicolon from functions

--- web-app/db.js
+++ web-app/db.js
@@ -18,7 +18,7 @@ function getChartData_24h(callback) {
 
         return callback(results.rows);
     });
-};
+}
 
 function getChartData_7d(callback) {
     pool.query('SELECT * FROM TPH WHERE TYPE = "AVG" AND EXTRACT (\'hour\' from TS) IN (0, 6, 12, 18) ORDER BY TIMESTAMP DESC LIMIT 28', (error, results) => {
@@ -29,7 +29,7 @@ function getChartData_7d(callback) {
 
         return callback(results.rows);
     });
-};
+}
 
 function getChartData_28d(callback) {
     pool.query('SELECT * FROM TPH WHERE TYPE = "AVG" AND EXTRACT (\'hour\' from TS) = 12 ORDER BY TIMESTAMP DESC LIMIT 28', (error, results) => {
@@ -40,15 +40,15 @@ function getChartData_28d(callback) {
 
         return callback(results.rows);
     });
-};
+}
 
 function putChartData(ts, type, temperature, pressure, humidity) {
     console.log('Inserting record');
-    
+
     pool.query('INSERT INTO TPH (ts, type, temperature, pressure, humidity) VALUES ($1, $2, $3, $4, $5)', [ts, type, temperature, pressure, humidity], (error, results) => {
         if (error) {
             console.log("Error inserting chart data");
             throw error;
         }
     });
-};
+}
