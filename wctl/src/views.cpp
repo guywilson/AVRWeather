c1567006095d17fd93af2992dea24e5920a7c0c5
Guy Wilson <wilson.guy@gmail.com>
2019-08-02 17:21:12 +0100

    Added delete to prevent memory leak

--- wctl/src/views.cpp
+++ wctl/src/views.cpp
@@ -85,6 +85,8 @@ void avrCommandHandler(struct mg_connection * connection, int event, void * p)
 						memcpy(szVersion, pRxFrame->getData(), pRxFrame->getDataLength());
 						szVersion[pRxFrame->getDataLength()] = 0;
 
+						delete pRxFrame;
+						
 						mg_printf(connection, "HTTP/1.1 200 OK\n\n Version [%s]", szVersion);
 						connection->flags |= MG_F_SEND_AND_CLOSE;
 					}
