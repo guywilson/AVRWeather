c8f97a9717fd68e111205c69671166e00627f00b
Guy Wilson <wilson.guy@gmail.com>
2019-09-25 08:00:30 +0100

    Fixed upload.sh

--- avr/upload.sh
+++ avr/upload.sh
@@ -2,9 +2,14 @@
 
 DEVICE=$1
 TARGET=$2
-#BAUDRATE=57600
 BAUDRATE=115200
+SERIALPORT=/dev/ttyACM0
 
-SERIALPORT="$(ls /dev/tty.* | grep usb)"
+toggle -pin 12
 
-avrdude -Cavrdude.conf -v -p$DEVICE -carduino -P$SERIALPORT -b$BAUDRATE -D -Uflash:w:$TARGET:i
+if [ $? -ne 0 ]; then
+    echo "Failed to toggle reset pin, did you forget to run with sudo?"
+    exit $?
+fi
+
+avrdude -C avrdude.conf -v -p $DEVICE -c arduino -P $SERIALPORT -b $BAUDRATE -D -U flash:w:$TARGET:i
