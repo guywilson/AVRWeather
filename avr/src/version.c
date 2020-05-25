633481f5815c91fdbda33eb69e9e3b5f529752c3
Guy Wilson <wilson.guy@gmail.com>
2020-05-25 17:13:24 +0100

    Updated version number

--- avr/src/version.c
+++ avr/src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2020-05-24 09:40:39"
-#define __BVERSION__   "1.5.004"
+#define __BDATE__      "2020-05-25 17:11:26"
+#define __BVERSION__   "1.5.005"
 
 const char * getVersion()
 {
